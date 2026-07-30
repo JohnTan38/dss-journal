2cff5410c689f588b1c78a12ec6cc7b8ffa70c17
John <appy1828@gmail.com>
2026-07-30 11:56:12 +0800

    Add files via upload

---
+++ data/new-content-journal-2.ts
@@ -0,0 +1,31 @@
+export const newContentJournal2SourceName = 'new-content-journal-2.txt';
+
+export const newContentJournal2Raw = `M3 - People management
+----------------------
+Learning Unit 1 -> Abilities (A10) within "Implementation & Support Actions"
+add text "Center has preference to recruit trained people with relevant prior experience example social worker, physiotherapist
+Typical career progression: Program Executive -> Senior Program Executive -> Center Manager (after 3-4 years)"
+--------------------------------------------------------------------------------------------------------------------
+Learning Unit 2 -> Knowledge (A7) -> after "Maintain Legal & Policy Compliance"
+add text "Center conducts annual performance review -> development & training to fill gaps in skillset -> attend courses -> AIC direction and according to organizational needs"
+--------------------------------------------------------------------------------------------------------------
+M4 - Volunteer Management
+-------------------------
+Learning Unit 1 -> Knowledge (K1) within "2. Recruitment Needs Analysis (RNA) section"
+add text "Two main sources - 1) volunteer seniors from AAC, 2) external volunteers
+Yes. Center performs Recruitment Needs Analysis (RNA) to discover gaps in service and how volunteers could fulfill them"
+-----------------------------------------------------------------------------------------------------------------------
+Learning Unit 1 -> Knowledge (K1) after "Volunteer Management Best Practices: Summary"
+add text "Center tracks hours served - hosts annual appreciation to thank volunteers - for example celebrate 5 or 10 years milestones"
+------------------------------------------------------------------------------------------------------------------
+Learning Unit 2 -> Abilities (A3 & A9) within "Comprehensive Needs Analysis and Volunteer Orientation"
+add text "Yes Center screens volunteers to align abilities and skillsets to organizational needs"
+-------------------------------------------------------------------------------------------------
+Learning Unit 2 -> Abilities (A3 & A9) after "Joint Training: A Model for Cost Efficiency and Standardization"
+add text "Center optimizes usage of resources by collaborating with other sister centers for activities -> 
+for example KWSC McNair collaborates with KWSC Jalan Besar for a Fun Walk activity -> 
+volunteers enjoy a new different activity -> a "cross-cultural exchange" of sorts -> under auspicious of HQ volunteer management"
+---------------------------------------------------------------------------------------------------------------------
+Learning Unit 3 -> Abilities (A8)
+add text "Center shared that volunteers are appointed to lead a program if skills align with organizational goals, aspirations, and volunteer time commitment"
+------------------------------------------------------------------------------------------------------------------------`;
